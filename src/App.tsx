58a358c074ef12bd611c113258168147dc791931
Anna Viklund <anna.viklund@hotmail.com>
2019-05-04 23:05:14 +0200

    Use prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Palett-palatset
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Palett-palatset</p>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
